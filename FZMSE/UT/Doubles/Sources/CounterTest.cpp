30c5fab9fc0d5bebba456271b611d4378041a123
pkozuch <piotr.kozuch@nokia.com>
2015-07-17 15:35:11 +0200

    TinyXML Tests add

--- FZMSE/UT/Doubles/Sources/CounterTest.cpp
+++ FZMSE/UT/Doubles/Sources/CounterTest.cpp
@@ -21,7 +21,7 @@ TEST(Counter, Increment) {
   cout << "Congratulations, you have just run a test :-)" << endl;
 }
 
-int main(int argc, char **argv) {
-   ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
+//int main(int argc, char **argv) {
+//   ::testing::InitGoogleTest(&argc, argv);
+//  return RUN_ALL_TESTS();
+//}
