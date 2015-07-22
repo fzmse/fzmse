a9fc15b4f6d1bd5e14b1ab0ef4b984e1b0511ea9
pfert <pfert@CNU317BPMF.nsn-intra.net>
2015-07-22 14:13:20 +0200

    XmlReader .h .cpp

---
+++ FZMSE/Includes/Xml/XmlReader.h
@@ -0,0 +1,22 @@
+#pragma once;
+
+#include "TinyXML/tinyxml2.h"
+#include <vector>
+#include <string>
+using namespace tinyxml2;
+
+class XmlReader {
+protected:
+	XMLElement* currElement;
+public:
+	XmlReader(XMLDocument* doc);
+	XmlReader(XMLElement* doc);
+	virtual ~XmlReader();
+	XMLElement * getCurrElement();
+	std::vector <XMLElement*> getChildrens(std::string tag);
+	XMLElement * getFirstChildren(std::string tag);
+	XMLElement * getChildrenByAttr(std::string tag, std::string attr, std::string value);
+	void setCurrElement(XMLElement * newElement);
+	void setNextElement();
+
+};
---
+++ FZMSE/Sources/Xml/XmlReader.cpp
@@ -0,0 +1,67 @@
+/*
+ * XmlReader.cpp
+ *
+ *  Created on: 22 lip 2015
+ *      Author: pfert
+ */
+
+#include "Xml/XmlReader.h"
+
+XmlReader::XmlReader(XMLDocument* doc)
+{
+	this->currElement = doc->RootElement();
+}
+
+XmlReader::XmlReader(XMLElement* element)
+{
+	this->currElement = element;
+}
+
+XmlReader::~XmlReader()
+{
+}
+
+
+XMLElement* XmlReader::getCurrElement()
+{
+	return this->currElement;
+}
+
+std::vector<XMLElement*> XmlReader::getChildrens(std::string tag = "")
+{
+	std::vector<XMLElement*> childrens;
+	XMLElement* currChildElement;
+	if(tag == "")
+		XMLElement* currChildElement = this->currElement->FirstChildElement();
+	else
+		XMLElement* currChildElement = this->currElement->FirstChildElement(tag.c_str());
+
+	while(currChildElement != NULL)
+	{
+		childrens.push_back(currChildElement);
+		currChildElement = currChildElement->NextSiblingElement();
+	}
+	return childrens;
+}
+
+XMLElement* XmlReader::getFirstChildren(std::string tag)
+{
+	XMLElement * a;
+	return a;
+}
+
+
+XMLElement* XmlReader::getChildrenByAttr(std::string tag, std::string attr,
+		std::string value)
+{
+	XMLElement * a;
+	return a;
+}
+
+void XmlReader::setCurrElement(XMLElement* newElement)
+{
+}
+
+void XmlReader::setNextElement()
+{
+}
